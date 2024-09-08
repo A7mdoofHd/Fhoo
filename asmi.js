bdb4c89e3968afd5c8fe7a9fe2aacae8087c5d93
A7mdoofHd <a7med.mads@gmail.com>
2024-09-08 23:07:47 +0300

    Create asmi.js

---
+++ asmi.js
@@ -0,0 +1,21 @@
+var ahmed=setInterval(function(){ 
+    if(myid!=null){ 
+        clearInterval(ahmed); 
+        $("#users .uzr:contains('أحمد')").addClass('ahm'); 
+		  $("#users .uzr.ahm span.uhash, #users .uzr.ahm img.co,#users .uzr.ahm .ustat,.fl.mini.u-co").hide(); 
+        
+
+    } 
+}, 5000); 
+
+
+$("style").last().append(` 
+#users .uzr.ahm .fitimg.u-pic { 
+  border-radius: 0px 6px 6px 0px!important; 
+} 
+#users .uzr.ahm { 
+    background-color:#E0E0E0; 
+} 
+#users .uzr.ahm .u-topic { 
+  margin-left: 5px; 
+} 
