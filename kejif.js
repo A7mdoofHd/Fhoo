b33b1554fbf6cae0693715c482e882d130c4a984
A7mdoofHd <a7med.mads@gmail.com>
2024-09-09 01:36:52 +0300

    Create kejif.js

---
+++ kejif.js
@@ -0,0 +1,10 @@
+
+let ahmed=setInterval(function(){
+    if(myid!=null){
+        clearInterval(ahmed);
+        $("#users.uzr:contains('أحمد')").addClass('ahm');
+		 
+        
+
+    }
+}, 1000);
